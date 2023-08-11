5b8b1194b707829230ddbc89e265d0517d352334
sreedhar <sreedharvsr228@gmail.com>
2023-08-11 14:38:06 +0530

    added log4j

--- src/main/java/com/vsr/jurisdiction/controller/ApiControllerImpl.java
+++ src/main/java/com/vsr/jurisdiction/controller/ApiControllerImpl.java
@@ -2,6 +2,8 @@ package com.vsr.jurisdiction.controller;
 
 import java.util.List;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -15,34 +17,36 @@ public class ApiControllerImpl implements ApiController {
 	
 	@Autowired
 	private ApiService service;
+	
+	Logger log = LogManager.getLogger() ;
 
 	@Override
 	public ResponseEntity<Jurisdiction> create(Jurisdiction jurisdiction) {
-		
+		log.info("Inside create method.(ApiControllerImpl)........!");
 		return new ResponseEntity(service.create(jurisdiction), HttpStatus.OK);
 	}
 
 	@Override
 	public ResponseEntity<List<Jurisdiction>> getData() {
-		
+		log.info("Inside getData method.(ApiControllerImpl)........!");
 		return new ResponseEntity(service.getData(), HttpStatus.OK);
 	}
 
 	@Override
 	public ResponseEntity<Jurisdiction> getDataById(int id) {
-		
+		log.info("Inside getDataById method.(ApiControllerImpl)........!");
 		return new ResponseEntity(service.getDataById(id), HttpStatus.OK);
 	}
 
 	@Override
 	public ResponseEntity<Jurisdiction> updateById(int id, Jurisdiction jurisdiction) {
-		
+		log.info("Inside updateById method.(ApiControllerImpl)........!");
 		return new ResponseEntity(service.updateById(id, jurisdiction), HttpStatus.OK);
 	}
 
 	@Override
 	public ResponseEntity<Jurisdiction> delete(int id) {
-		
+		log.info("Inside delete method.(ApiControllerImpl)........!");
 		return new ResponseEntity(service.delete(id), HttpStatus.OK);
 	}
 
--- src/main/java/com/vsr/jurisdiction/exception/ApiException.java
+++ src/main/java/com/vsr/jurisdiction/exception/ApiException.java
@@ -1,5 +1,7 @@
 package com.vsr.jurisdiction.exception;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -9,9 +11,10 @@ import com.vsr.jurisdiction.model.ErrorResponse;
 
 @ControllerAdvice
 public class ApiException {
-	
+	Logger log = LogManager.getLogger() ;
 	@ExceptionHandler(Throwable.class)
 	public ResponseEntity<ErrorResponse> HandleException(Throwable ex){
+		log.info("Inside HandleException method.(ApiException)........!");
 		return new ResponseEntity<>(getResponse(ex), HttpStatus.INTERNAL_SERVER_ERROR);
 		
 	}
@@ -19,6 +22,7 @@ public class ApiException {
 	 ErrorResponse response = new ErrorResponse();
 	 response.setStatusCode("5XX");
 	 response.setMessage(ex.getMessage());
+	 log.info("Error-Message : "+ ex.getMessage());
 	 response.setReason(ex.getClass().getName());
 	return response;
  }
